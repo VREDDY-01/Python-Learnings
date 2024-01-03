a182700cb1c021386e4f23b4d26dedbd0b7f656a
K Vishnu Teja <vreddy2300@gmail.com>
2024-01-03 07:41:26 +0530

    create calculator excersice

---
+++ 3-Excersice-1/calculator.py
@@ -0,0 +1,24 @@
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def multiply(a,b):
+    print(a*b)
+def divide(a,b):
+    print(a/b)
+def square(a,b):
+    print(a**b)
+def floorDiv(a,b):
+    print(a//b)
+def mod(a,b):
+    print(a%b)
+    
+    
+add(1,2)
+sub(2,1)
+multiply(2,8)
+divide(6,2)
+square(5,2)
+floorDiv(7,3)
+mod(2,1)
+    
