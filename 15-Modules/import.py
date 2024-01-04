0eee0713a0ff95f725af8b3bb34775b75363f75f
K Vishnu Teja <vreddy2300@gmail.com>
2024-01-04 09:29:55 +0530

    Modules and imports and name == main

---
+++ 15-Modules/import.py
@@ -0,0 +1,14 @@
+# # import pandas
+# import math as m
+
+# from math import *
+from math import sqrt
+from vishnu import hello
+
+
+# # pandas.read_csv()
+# print(m.floor(4.588123))
+# print(m.sqrt(9))
+
+hello("Vishnu")
+print(sqrt(9))
---
+++ 15-Modules/vishnu.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"hello {name}")
+
+if __name__ == "__main__":   #only executes if we execute tis script particularly
+    hello("vishnu")
