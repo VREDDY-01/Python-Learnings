188e10274e31ebed76ef9c69dc493ab8751407c2
K Vishnu Teja <vreddy2300@gmail.com>
2024-01-04 09:07:12 +0530

    micelleneuos concepts enumerate and sharthand ifelse

---
+++ 10-MisceleniousConcepts(fString,docStrings)/enumerate.py
@@ -0,0 +1,4 @@
+marks = [12,36,89,65,99,54]
+
+for index,mark in enumerate(marks):
+    print(f"The marks of {index} student is {mark}.")
---
+++ 10-MisceleniousConcepts(fString,docStrings)/ifelseShorthand.py
@@ -0,0 +1,2 @@
+a = int(input("Enter a number"))
+print("hello") if a<10 else print("Good") if a==10 else print("Bad")
